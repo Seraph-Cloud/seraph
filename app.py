655f69f404f029c52c210f14c916d4f8cafcf41e
Richard Neff <div3y3z@gmail.com>
2022-02-05 02:12:47 -0800

    Continued appification

--- app.py
+++ app.py
@@ -4,18 +4,19 @@ from cProfile import run
 # Flask app configuration and launcher
 import sqlite3 # required
 from flask import Flask, render_template
-# comment to db first
+# connect to db first
+# we are using Sqlite and php for web coding with flask
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 from seraph_main import *
-#def seraphAppLauncher():
-#    return "Seraph is loading..."
-#def seraphBuild():
-#    return "Seraph is rebuilding indexes...", exec('seraph_main.py')
-#def seraphWebAppLauncher():
-#    return "Launching Web App on Localhost" # do stuff 
+def seraphAppLauncher():
+    return "Seraph is loading..."
+def seraphBuild():
+    return "Seraph is rebuilding indexes...", exec('seraph_main.py')
+def seraphWebAppLauncher():
+    return "Launching Web App on Localhost" # do stuff 
 app = Flask(__name__)
 @app.route('/')
 def index():
