43c8d2d009b897ac49274565434d8bee2f1cb61a
Richard Neff <div3y3z@gmail.com>
2022-02-04 16:56:11 -0800

    updating vars script

--- example.seraph_vars.py
+++ example.seraph_vars.py
@@ -1,20 +1,39 @@
+# Seraph
+# Author: @diveyez
+# This is the main variables file for the application
 # ----------------Do not store in cloud------------------- #
-TOKEN1 = '<APITOKEN>'
-#TOKEN2 = '<APITOKEN>'
-#TOKEN3 = '<APITOKEN>'
-#TOKEN4 = '<APITOKEN>'
 # FOR TESTING
-# SYMBOL= 'AMD'
-# ----------------Do not store in cloud------------------- #
+SYMBOL = open('seraph_pid.txt', "r")
+SYMBOL.read()
+SYMBOL
+SYMBOL.close()
+# TDA Api  Info
+TDClient = ''
+CLIENT_ID = ''
+REDIRECT_URI = ''
+CREDENTIALS_FILE = ''
+# Twelvedata Tokens
+TOKEN1 = ''
+TOKEN2 = ''
+#TOKEN3 = ''
+#TOKEN4 = ''
+YAHOO_TOKEN = ''
+# twelvedata charts
 chart_test = 'data/images/test.png'
-ticker_symbol = 'seraph_pid.txt'
-chart_1min_1 = 'data/images/time_series_plots/1min/1.1'
+chart_1min_1 = 'data/images/time_series_plots/1min/'
 chart_1min_2 = 'data/images/time_series_plots/1min/'
 chart_5min_1 = 'data/images/time_series_plots/5min/'
 chart_5min_2 = 'data/images/time_series_plots/5min/'
 chart_15min_1 = 'data/images/time_series_plots/15min/'
 chart_15min_2 = 'data/images/time_series_plots/15min/'
-#
-# Test Below More
+# FINVIZ
+finviz_asset = 'data/images/finviz/'
 # ----------------Do not store in cloud------------------- #
+# Test Below More
+#
 grid_plots = 'data/images/grid_plots/test.png'
+#
+# Options Data Pulls
+options_calls = 'data/json/calls'
+options_puts = 'data/json/puts'
+# ----------------Do not store in cloud------------------- #
