f9a52349b937fd3d7ce8c015ef875e3ca876bef2
Richard Neff <div3y3z@gmail.com>
2022-02-04 20:22:09 -0800

    Update seraph_ui.py

--- seraph_ui.py
+++ seraph_ui.py
@@ -1,11 +1,12 @@
 def menu():
-
+    print("--------------Seraph Financials-------------------")
     print("[1] 1,5,15,30 min Plotly Charts (with TwelveData) ")
     print("[2] Options Chain Data (with TwelveData)")
     print("[3] Daily Charts (with Finviz)")
     print("[4] Pull List of Stocks")
     print("[5] Pull Stock Quote (Yahoo)")
     print("[6] Pull Sec.gov Filings")
+    print("--------------------------------------------------")
     print ("[0] Quit")
 menu()
 print("all data located in /data/ folder")
