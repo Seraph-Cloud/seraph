fdffcebe2d8ce5ba7067672af5482a8f8116ffd2
Richard Neff <div3y3z@gmail.com>
2022-02-05 03:26:28 -0800

    page/post creation fnc

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ def seraphBuild():
 def seraphWebAppLauncher():
     return "Launching Web App on Localhost" # do stuff 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '-dsbI1HJft672.fd2y8'
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -48,3 +49,20 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
