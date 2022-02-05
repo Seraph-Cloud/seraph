338dd189f4c44f4af83d3355c941488fb9b52ab2
Richard Neff <div3y3z@gmail.com>
2022-02-04 18:15:25 -0800

    Update input.py

--- modules/input.py
+++ modules/input.py
@@ -17,8 +17,11 @@ import requests
 import os
 import time
 import sys
-# Setting up the absolue path
+# Setting up the absolue path & app data paths
 drive, tcase_dir = os.path.splitdrive(os.path.abspath(__file__))
+paths = tcase_dir.split(os.sep)[:-2]
+base_dir = os.path.join(drive,os.sep,*paths)
+
 # Twelvedata Token
 SELECTED_TOKEN = input("What Token:")
 # Write to file
@@ -32,7 +35,7 @@ print("Imported Vars and Modules....")
 # TICKER SELECTION
 ticker_symbol = input("What Ticker:")
 # Write to file
-f = open('/seraph_pid.txt', "w")
+f = open('seraph_pid.txt', "w")
 f.write(
         ticker_symbol
 )
