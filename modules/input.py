b67cc421e681a8c2294d4533dcbca9a7c7cba748
Richard Neff <div3y3z@gmail.com>
2022-02-04 18:33:34 -0800

    Update input.py

--- modules/input.py
+++ modules/input.py
@@ -1,7 +1,7 @@
-# User Token and NYSE Symbol Selection Scipt
+# User Token and NYSE Symbol Selection Test Scipt
 # Author: @diveyez
-#
-from seraph_vars import *
+# adding more test code to this file
+# this is only for testing
 import plotly.graph_objects as go
 import nasdaqdatalink
 import mplfinance as mf
@@ -21,15 +21,28 @@ import sys
 drive, tcase_dir = os.path.splitdrive(os.path.abspath(__file__))
 paths = tcase_dir.split(os.sep)[:-2]
 base_dir = os.path.join(drive,os.sep,*paths)
+# Importing the rest of Seraph files
+import modules
+from seraph_vars import *
 data_dir = r'data'
 data_csv_dir = r'data/csv'
-data_db = r'data/db'
-images_dir = r'data/images'
+data_db_dir = r'data/db'
+data_images_dir = r'data/images'
+data_images_finviz_dir = r'data/images/finviz'
+data_images_time_series_plots_dir = r'data/images/time_series_plots'
+data_images_time_series_plots_1min_dir = r'data/images/time_series_plots/1min'
+data_images_time_series_plots_5min_dir = r'data/images/time_series_plots/5min'
+data_images_time_series_plots__15min_dir = r'data/images/time_series_plots/15min'
+data_images_time_series_plots_30min_dir = r'data/images/time_series_plots/30min'
+data_images_time_series_plots_dir = r'data/images/time_series_plots'
+seraph_abs_path = os.path.join(base_dir, data_dir, data_csv_dir, data_db_dir, data_images_dir, data_images_time_series_plots_dir )
+# Example Defining
+# <something>_abs_path = os.path.join(base_dir, dir2, dir3)
 
 # Twelvedata Token
 SELECTED_12dTOKEN = input("What Token:")
 # Write to file
-f = open('selected_token.txt', "w")
+f = with open(os.path.join())('selected_token.txt', "w")
 f.write(
     SELECTED_12dTOKEN
 )
