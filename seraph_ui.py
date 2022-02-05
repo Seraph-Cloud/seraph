e0b230bda65441d2ffd7a53847a93387e01a5229
Richard Neff <div3y3z@gmail.com>
2022-02-04 23:53:42 -0800

    .

--- seraph_ui.py
+++ seraph_ui.py
@@ -1,10 +1,3 @@
 # Seraph UI
 # Author: @diveyez
-# we are using tkinter because it is built
-# into python and maskes a very quick way to
-# dockerize this as a web application
-from tkinter import *
-# reach the internet
-import webbrowser
-# Constructing the UI
-
+# using Kivy as a GUI
