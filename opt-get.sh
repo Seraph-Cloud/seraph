23d9c6795112f65751e2475a3a5a53592b6872ff
R. Neff <29211141+diveyez@users.noreply.github.com>
2022-03-02 10:52:09 -0800

    Create opt-get.sh

---
+++ opt-get.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Get the options chain data and store it
+# Example line
+# curl -X GET --header "Authorization: " "https://api.tdameritrade.com/v1/marketdata/chains"
+#
+#
+#
+#
+#
+echo "Done getting Options Chain Data for $symbol";
